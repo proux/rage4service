42e5a77c1f4d442b0e6191c7c79e5d6da3263168
Nico Lindemann <nico.lindemann@proux.net>
2016-05-08 18:45:29 +0200

    test(coverage): set up test coverage tools

--- test/index_spec.js
+++ test/index_spec.js
@@ -1,14 +1,36 @@
 var expect = require('chai').expect
 var r4s = require('../lib/index.js')
+var nock = require('nock')
 
-it('index method should return api response with utctime and version', function (done) {
+it('index method should return api response with utctime and version', function () {
   this.timeout(5000)
+
+  nock('https://secure.rage4.com/RAPI/')
+    .persist()
+    .get(/index/)
+    .query(true)
+    .reply(200, {
+      utctime: 'now',
+      version: 'tester'
+    })
+
   r4s.index().then(function (data) {
     expect(data).to.have.property('utctime')
     expect(data).to.have.property('version')
-    done()
-  }, function (error) {
-    assert.fail(error)
-    done()
+  })
+})
+
+it('getdomains method should return array', function () {
+  this.timeout(5000)
+
+  nock('https://secure.rage4.com/RAPI/')
+    .persist()
+    .get(/getdomains/)
+    .query(true)
+    .reply(200, [])
+
+  r4s.getdomains().then(function (data) {
+    console.log(data)
+    expect(data).to.be.a('array')
   })
 })
